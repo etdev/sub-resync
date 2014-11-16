001e5cf78edb937de00531ec604ac291d24858be
Eric Turner <ericturnerdev@gmail.com>
2014-11-16 14:24:30 +0900

    Basically working

--- subs.rb
+++ subs.rb
@@ -1,5 +1,8 @@
+require 'pp'
+
 #Re-time SRT files
 def init_file(file_name)
+  @in_filename = file_name
   puts "Opening #{file_name}..."
   file = File.open(file_name, "r")
   timings = {}
@@ -8,7 +11,7 @@ def init_file(file_name)
     if line =~ /-->/
       (/^.*([0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}) --> .*([0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}).*/).match(line)
       timings[i.to_s] = { start: $1, end: $2 }
-      i = i+1
+      i+=1
     end
   end
     puts "Found #{i-1} timing entries."
@@ -25,8 +28,9 @@ def ask_change
     puts "Do you want to edit the timings? (Y/n)"
     keep_asking = gets.chomp
     break if keep_asking == 'n' || keep_asking == 'N' || keep_asking == 'no' || keep_asking == 'No'
-    puts "Enter your change start time: "
+    puts "Enter your change start time ('0' = beginning, otherwise 'HH:MM:SS,mmm' format): "
     start = gets.chomp
+    start = '00:00:00,000' if start == '0'
     puts "Enter the time difference in seconds: "
     diff = gets.chomp
     if (diff[0] == '-')
@@ -34,8 +38,27 @@ def ask_change
     else
       retime(start, diff, '+')
     end
+    puts "Current timings (use these values if you want to edit again): "
+    pp @timings
   end
-  puts @timings
+end
+
+def rewrite_file
+  puts "Enter a filename to save your output: "
+  @out_filename = gets.chomp
+  in_file = File.open(@in_filename, 'r')
+  out_file = File.open(@out_filename, 'w')
+  i = 0
+  in_file.each_with_index do | line, index |
+    if line =~ /-->/
+      (/^.*([0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}) --> .*([0-9]{2}:[0-9]{2}:[0-9]{2},[0-9]{3}).*/).match(line)
+      out_file.puts("#{@timings[i.to_s][:start]} --> #{@timings[i.to_s][:end]}")
+      i+=1
+    else
+      out_file.puts(line)
+    end
+  end
+
 end
 
 def retime(start, diff, op)
@@ -97,6 +120,22 @@ def get_normalized(num_secs)
   return "#{fix_zeros(hours)}:#{fix_zeros(mins)}:#{fix_zeros(secs)},000"
 end
 
+def print_in_file
+  puts "Printing output file..."
+  in_file = File.open(@in_filename, "r")
+  in_file.each_with_index.map do | line, index |
+    puts line
+  end
+end
+
+def print_out_file
+  puts "Printing output file..."
+  out_file = File.open(@out_filename, "r")
+  out_file.each_with_index.map do | line, index |
+    puts line
+  end
+end
+
 def fix_zeros(val)
   if (val.to_s).length == 1
     val = '0' + (val.to_s)
@@ -127,6 +166,24 @@ def store_milis
     (/^([0-9]+):([0-9]+):([0-9]+),([0-9]+)$/).match(val[:end])
     temp_hash[:end] = $4
     @milis[index] = temp_hash
+
+  end
+end
+
+def restore_milis
+  @timings.each do |index, val|
+    if (/^([0-9]+):([0-9]+):([0-9]+),([0-9]+)$/).match(val[:start])
+      @timings[index][:start] = "#{$1}:#{$2}:#{$3},#{@milis[index][:start]}"
+    else
+      puts "ERROR: INVALID INPUT"
+      return 0
+    end
+    if (/^([0-9]+):([0-9]+):([0-9]+),([0-9]+)$/).match(val[:end])
+      @timings[index][:end] = "#{$1}:#{$2}:#{$3},#{@milis[index][:end]}"
+    else
+      puts "ERROR: INVALID INPUT"
+      return 0
+    end
   end
 end
 
@@ -135,6 +192,9 @@ file_name = ask_file
 @timings = init_file(file_name)
 store_milis
 ask_change
+restore_milis
+rewrite_file
+puts "Finished storing your retimed subs to #{@out_filename}.\n"
 
 
 
